8b867b737662b6364e1c4802ee5d8dc539c68536
LAPTOP-EV2MC11S\ktakw <lull.koutakuwamura@gmail.com>
2025-08-07 13:27:34 +0900

    following.html大枠完成、FollowController following()大枠完成

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -4,8 +4,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Post;
+import com.example.demo.entity.User;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 	// createdAt（作成日時）で降順に並べる
 	List<Post> findAllByOrderByCreatedAtDesc();
+	// 指定されたユーザーの投稿を取得（作成日時で降順）
+	List<Post> findByUserInOrderByCreatedAtDesc(List<User> users);
+
 }
